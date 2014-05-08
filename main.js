afe33c039f860453170e6ea8c9e311fa57e71349
Nick Walker <nwalker75@yahoo.com>
2014-05-08 11:36:06 -0600

    first commit. basic js methods being used on a user string.

---
+++ main.js
@@ -0,0 +1,17 @@
+var vacation = prompt("Where would you like to go on your next vacation?");
+
+alert("So, you're telling me you want to go to " + vacation +"." + " Awesome!");
+
+alert("The length of that word is " + vacation.length + " letters long");
+
+alert("Make it uppercase: " + vacation.toUpperCase());
+
+alert("Make it lowercase: " + vacation.toLowerCase());
+
+alert("What's the 4th letter of your destination, you ask... well, it's " + vacation.charAt(3) + " of course!");
+
+alert("I've been wanting to go to " + vacation + " since I was a little boy.")
+
+alert(vacation.substring(1,4));
+
+
